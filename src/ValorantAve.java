49d68d459345c4bdb089e0bb92f9a81c587bde52
pukusyou <pukusho@gmail.com>
2021-06-15 13:49:31 +0900

    コメントアウト完成

--- src/ValorantAve.java
+++ src/ValorantAve.java
@@ -1,29 +1,20 @@
+/**
+ * kd比平均を扱うクラス
+ * @author pukusyou
+ */
 public class ValorantAve{
 
 	private ValorantScore valorantScore;
 	private String name, team;
 	private double kdAve;
 
-	public void setKillScore(int[] killList){
-		for (int i = 0; i < killList.length; i++) {
-			valorantScore.setKillList(killList[i]);
-		}
-	}
-
-	public void setDeathScore(int[] deathList){
-		for (int i = 0; i < deathList.length; i++) {
-			valorantScore.setDeathList(deathList[i]);
-		}
-	}
-
-	public double calcKDAve() {
-		double sum = 0;
-		for (Double kd : valorantScore.getKDList()) {
-			sum += kd;
-		}
-		return sum / valorantScore.getKDList().size();
-	}
-
+	/**
+	 * コンストラクタ
+	 * @param name プレイヤー名
+	 * @param team チーム名
+	 * @param killList キル数一覧
+	 * @param deathList デス数一覧
+	 */
 	public ValorantAve(String name, String team, int[] killList, int[] deathList) {
 		this.name = name;
 		this.team = team;
@@ -34,29 +25,66 @@ public class ValorantAve{
 		kdAve = calcKDAve();
 	}
 
+	/**
+	 * 引数をvalorantScoreのsetKillListメソッドを用いて代入
+	 * @param killList 試合ごとのキル数
+	 */
+	public void setKillScore(int[] killList){
+		for (int i = 0; i < killList.length; i++) {
+			valorantScore.setKillList(killList[i]);
+		}
+	}
+
+	/**
+	 * 引数をvalorantScoreのsetDeathListメソッドを用いて代入
+	 * @param deathList 試合ごとのデス数
+	 */
+	public void setDeathScore(int[] deathList){
+		for (int i = 0; i < deathList.length; i++) {
+			valorantScore.setDeathList(deathList[i]);
+		}
+	}
+
+	/**
+	 * kd比の平均を計算し、返します
+	 * @return 直近数試合のkd比の平均
+	 */
+	public double calcKDAve() {
+		double sum = 0;
+		for (Double kd : valorantScore.getKDList()) {
+			sum += kd;
+		}
+		return sum / valorantScore.getKDList().size();
+	}
+
+	/**
+	 * kd比の平均を表示します
+	 */
 	public void showKDAve() {
 		System.out.printf("%sの最近%d試合の平均KDは%.2fです\n", name, valorantScore.getKDList().size(), kdAve);
 	}
 
+	/**
+	 * kdAveのゲッター
+	 * @return kdAve
+	 */
 	public double getKDAvg() {
 		return kdAve;
 	}
 
+	/**
+	 * teamのゲッター
+	 * @return team
+	 */
 	public String getTeam() {
 		return team;
 	}
 
+	/**
+	 * nameのゲッター
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
-
-//	public int compareTo(ValorantAve o) {
-//		if(this.kdAve < o.kdAve) {
-//			return -1;
-//		}
-//		if(this.kdAve > o.kdAve) {
-//			return 1;
-//		}
-//		return 0;
-//	}
 }
